cc214978e3ff48953f1d53fc27c44b449524b59e
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-02-20 12:21:11 +0100

    Setup Real Estate App Project

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl my-10 font-rubik">
+        Welcome to ReState
+      </Text>
+      <Link href="/sign-in">
+        <Text>Sign In</Text>
+      </Link>
+      <Link href="/explore">
+        <Text>Explore</Text>
+      </Link>
+      <Link href="/properties/1">
+        <Text>Properties</Text>
+      </Link>
+      <Link href="/profile">
+        <Text>Profile</Text>
+      </Link>
+    </View>
+  );
+}
