55cc8f5e683e6f1b22358f03583349ddc6ee0ed4
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-02-20 13:18:14 +0100

    feat(appwrite): Implement Google OAuth login and user management
    - Added function for OAuth2 authentication with Google
    - Implemented function to delete current user session
    - Created function to retrieve user information and generate avatar
    - Enhanced error handling across all functions for better debugging
    - Improved code organization and readability

---
+++ lib/appwrite.ts
@@ -0,0 +1,92 @@
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
+
+export const config = {
+  platform: "com.primecode.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+
+export const account = new Account(client);
+
+export async function loginWithGoogle() {
+  try {
+    const redirectUri = Linking.createURL("/");
+
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+
+    if (!response) {
+      throw new Error("Failed to login with Google");
+    }
+
+    const browserResponse = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+
+    if (!browserResponse.type || browserResponse.type !== "success") {
+      throw new Error("Failed to login with Google");
+    }
+
+    const url = new URL(browserResponse.url);
+
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+
+    if (!secret || !userId) {
+      throw new Error("Failed to login with Google");
+    }
+
+    const session = await account.createSession(userId, secret);
+
+    if (!session) {
+      throw new Error("Failed to login with Google");
+    }
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const user = await account.get();
+
+    if (user.$id) {
+      const userAvatar = avatar.getInitials(user.name);
+
+      return {
+        ...user,
+        avatar: userAvatar.toString(),
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
